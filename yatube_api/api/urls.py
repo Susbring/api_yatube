63f56d2488acc486aeaf256c8b07a83d6c40a277
Denis Zuev <zuyev2001@yandex.ru>
2025-01-30 18:46:05 +0700

    Commit after rew

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,21 +1,20 @@
 from django.urls import include, path
-
 from rest_framework.authtoken import views
 from rest_framework.routers import DefaultRouter
 
 from .views import PostViewSet, GroupViewSet, CommentViewSet
 
-router = DefaultRouter()
-router.register('posts', PostViewSet)
-router.register('groups', GroupViewSet)
-router.register(
+router_v1 = DefaultRouter()
+router_v1.register('posts', PostViewSet)
+router_v1.register('groups', GroupViewSet)
+router_v1.register(
     r'posts/(?P<post_id>\d+)/comments',
     CommentViewSet,
-    basename="comment"
+    basename='comment'
 )
 
 
 urlpatterns = [
-    path('v1/', include(router.urls)),
+    path('v1/', include(router_v1.urls)),
     path('v1/api-token-auth/', views.obtain_auth_token),
 ]
--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -4,10 +4,20 @@ from django.db import models
 User = get_user_model()
 
 
+GROUP_TITLE_MAX_LENGTH = 200
+POST_IMAGE_UPLOAD_TO = 'posts/'
+COMMENT_CREATED_VERBOSE_NAME = 'Дата добавления'
+POST_PUB_DATE_VERBOSE_NAME = 'Дата публикации'
+
+
 class Group(models.Model):
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=GROUP_TITLE_MAX_LENGTH)
     slug = models.SlugField(unique=True)
     description = models.TextField()
+
+    class Meta:
+        verbose_name = 'Группа'
+        verbose_name_plural = 'Группы'
 
     def __str__(self):
         return self.title
@@ -16,18 +26,23 @@ class Group(models.Model):
 class Post(models.Model):
     text = models.TextField()
     pub_date = models.DateTimeField(
-        'Дата публикации', auto_now_add=True
+        POST_PUB_DATE_VERBOSE_NAME, auto_now_add=True
     )
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name='posts'
     )
     image = models.ImageField(
-        upload_to='posts/', null=True, blank=True
+        upload_to=POST_IMAGE_UPLOAD_TO, null=True, blank=True
     )  # поле для картинки
     group = models.ForeignKey(
         Group, on_delete=models.SET_NULL,
         related_name='posts', blank=True, null=True
     )
+
+    class Meta:
+        verbose_name = 'Пост'
+        verbose_name_plural = 'Посты'
+        ordering = ['-pub_date']
 
     def __str__(self):
         return self.text
@@ -42,5 +57,13 @@ class Comment(models.Model):
     )
     text = models.TextField()
     created = models.DateTimeField(
-        'Дата добавления', auto_now_add=True, db_index=True
+        COMMENT_CREATED_VERBOSE_NAME, auto_now_add=True, db_index=True
     )
+
+    class Meta:
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
+        ordering = ['-created']
+
+    def __str__(self):
+        return self.text
